25cd399528e0dbcd967699661881616d8486b70f
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-11 16:48:46 +0100

    added method for getting responses from the server

--- src/DefaultYggdrasil.php
+++ src/DefaultYggdrasil.php
@@ -2,6 +2,10 @@
 namespace PublicUHC\PhpYggdrasil;
 
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Message\ResponseInterface;
+
 class DefaultYggdrasil implements Yggdrasil {
 
     const AUTH_SERVER_URL = 'https://authserver.mojang.com';
@@ -9,12 +13,33 @@ class DefaultYggdrasil implements Yggdrasil {
     private $username;
     private $clientToken;
     private $accessToken;
+    private $httpClient;
 
     public function __construct($username, $clientToken = null, $accessToken = null)
     {
         $this->username = $username;
         $this->clientToken = $clientToken;
         $this->accessToken = $accessToken;
+        $this->httpClient = new Client();
+    }
+
+    /**
+     * Get a response from the given subURL via POST with the given JSON data. Sets header Content-Type for JSON
+     *
+     * @param $subURL String the sub url to add onto AUTH_SERVER_URL
+     * @param $jsonData array the json payload
+     * @return ResponseInterface the returned response
+     */
+    private function getResponse($subURL, $jsonData) {
+        return $this->httpClient->post(
+            self::AUTH_SERVER_URL . $subURL,
+            [
+                'json'      => $jsonData,
+                'headers'   => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]
+        );
     }
 
     public function getClientToken()
